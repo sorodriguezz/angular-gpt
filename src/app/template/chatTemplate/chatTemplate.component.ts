e2dcc3d59ac59beb51db2bb0c7f5e04e7292023c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-01-25 11:25:35 -0300

    feat: se crea service y template component

--- src/app/template/chatTemplate/chatTemplate.component.ts
+++ src/app/template/chatTemplate/chatTemplate.component.ts
@@ -5,38 +5,30 @@ import {
   inject,
   signal,
 } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ChatMessageComponent } from '@components/chat-bubbles/chatMessage/chatMessage.component';
 import { MyMessageComponent } from '@components/chat-bubbles/myMessage/myMessage.component';
 import { TextMessageBoxComponent } from '@components/text-boxes/textMessageBox/textMessageBox.component';
-import {
-  TextMessageBoxFileComponent,
-  TextMessageEvent,
-} from '@components/text-boxes/textMessageBoxFile/textMessageBoxFile.component';
-import {
-  TextMessageBoxEvent,
-  TextMessageBoxSelectComponent,
-} from '@components/text-boxes/textMessageBoxSelect/textMessageBoxSelect.component';
 import { TypingLoaderComponent } from '@components/typingLoader/typingLoader.component';
 import { Message } from '@interfaces/message.interface';
-import { OpenaiService } from './../../services/openai.service';
+import { OpenaiService } from 'app/presentation/services/openai.service';
 
 @Component({
-  selector: 'app-orthography-page',
+  selector: 'app-chat-template',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ChatMessageComponent,
     MyMessageComponent,
     TypingLoaderComponent,
     TextMessageBoxComponent,
-    TextMessageBoxFileComponent,
-    TextMessageBoxSelectComponent,
   ],
-  templateUrl: './orthographyPage.component.html',
+  templateUrl: './chatTemplate.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent {
-  public messages = signal<Message[]>([{ text: 'Hello', isGpt: false }]);
+export class ChatTemplateComponent {
+  public messages = signal<Message[]>([]);
   public isLoading = signal<boolean>(false);
   public openAiService = inject(OpenaiService);
 
@@ -44,11 +36,11 @@ export default class OrthographyPageComponent {
     console.log({ prompt });
   }
 
-  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
-    console.log({ prompt, file });
-  }
+  // handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+  //   console.log({ prompt, file });
+  // }
 
-  handleMessageWithSelect(event: TextMessageBoxEvent) {
-    console.log(event);
-  }
+  // handleMessageWithSelect(event: TextMessageBoxEvent) {
+  //   console.log(event);
+  // }
 }
