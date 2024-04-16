32a39030a12f1aeffe306cb287bae12d4b0920ca
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-04-16 18:20:13 -0400

    se crea ventana pros y cons

--- src/app/presentation/services/openai.service.ts
+++ src/app/presentation/services/openai.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { orthographyUseCase } from '@use-cases/orthography/orthography.use-case';
+import { prosConsUseCase } from '@use-cases/pros-cons/pros-cons.use-case';
 import { from } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class OpenaiService {
   checkOrthography(prompt: string) {
     return from(orthographyUseCase(prompt));
+  }
+
+  prosConsDiscusser(prompt: string) {
+    return from(prosConsUseCase(prompt));
   }
 }
